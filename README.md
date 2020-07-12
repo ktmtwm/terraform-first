5d6aa173d69ea76ce860696c86e2b937ea24b0d7
ktmtwm <agile@tanwenmin.top>
2020-07-12 22:17:04 +1200

    add more details in README.md && enable OS/Firewall

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # terraform-first
-terraform init
-
-terraform plan
-
-terraform apply 
-
-//terraform apply \\
-//  -var-file="production.tfvars"
+Execute with command: 
+	terraform init
+	terraform plan
+	terraform apply 
+You will get output of public_id:
+	Outputs:
+		public_ip = xx.xx.xx.xx
+View Nginx server with:
+	Home page:
+		http://<public_ip>
+	Resource usage page:	(Realtime refresh)
+		http://<public_ip>/resource.html
+	Health check page:		(Realtime refresh)
+		http://<public_ip>/health.html
+	Nginx root page words calculate:
+		http://<public_ip>/words.html
 
 terraform destroy
--- docker-scripts/install-docker-nginx.sh
+++ docker-scripts/install-docker-nginx.sh
@@ -1,16 +1,40 @@
 #!/bin/sh
+# install docker ce
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker ubuntu
 echo added user to docker group
+
+# install nginx
 docker pull nginx:latest
+# start nginx with health check and volume
 docker run --health-cmd='curl -sS http://127.0.0.1 || exit 1' \
     --health-timeout=10s \
     --health-retries=3 \
     --health-interval=5s \
+    -v /tmp:/usr/share/nginx/html/tmp \
 	--name nginx -p 80:80 -d nginx 
 
-nohup sudo docker logs -f nginx >> /tmp/nginx-health.log &
-nohup sudo docker stats nginx >> /tmp/resource.html &
-docker cp nginx:/usr/share/nginx/html/index.html /tmp/
-python3 /tmp/calculate_words.py | sudo tee -a /tmp/index-calc.log
+# change nginx configuration, show resource.log
+docker cp nginx:/etc/nginx/conf.d/default.conf /tmp/
+cp /tmp/default.conf /tmp/default.conf.org
+head -n -2 /tmp/default.conf.org |sudo tee /tmp/default.conf
+echo "location = /resource.html { root /usr/share/nginx/html/tmp;}
+	  location = /health.html { root /usr/share/nginx/html/tmp;}
+	  location = /words.html { root /usr/share/nginx/html/tmp;}
+	  }" >> /tmp/default.conf
+docker cp /tmp/default.conf nginx:/etc/nginx/conf.d/
+docker restart nginx
 
+# health check log
+nohup sudo docker logs -f nginx >> /tmp/health.html &
+
+# Resource usage log
+nohup sudo docker stats nginx >> /tmp/resource.html &
+
+# Nginx default page words count
+docker cp nginx:/usr/share/nginx/html/index.html /tmp/
+python3 /tmp/calculate_words.py | sudo tee -a /tmp/words.html
+
+# Ubuntu firewall setting
+ufw allow https
+ufw enable
